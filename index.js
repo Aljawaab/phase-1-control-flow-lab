188367d21c5bfcfa8b190e30e162b61f7dfcf51c
Abdul Aljawaab <abdulkhamis4041@gmail.com>
2024-09-25 13:14:05 +0300

    coded_1st_cntrlFlow

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return "This one is on me!"
+  } else if (someValue <= 2000){
+    return 'That will be twenty bucks.'
+  } else if (someValue <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue => 2501) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+   return City === 'NYC'? "Ok, sounds good.": 'No go.';// Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(generous){
+  switch (generous){
+    case 'generous':
+      return 'Thank you so much.'
+    case "not as generous":
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }// Write your code here!
+  
 }
